64a9b22213fdf802633ee129f5f557204a54fe96
josidd <joseph.siddons@noc.ac.uk>
2025-05-20 08:13:09 +0100

    chore: update version to release candidate

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.12.0 (unreleased)
+## 1.0.0 (unreleased)
 
 Contributors to this version: Joseph Siddons (@josidd)
 
--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../"))
 project = "GeoTrees"
 copyright = "2024, National Oceanography Centre"
 author = "NOC Surface Processes"
-release = "0.11.2"
+release = "1.0.0-rc.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

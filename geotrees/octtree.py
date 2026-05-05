fd9b5ffa520b6ff54d62a87750f5a2e8915989c3
josidd <joseph.siddons@noc.ac.uk>
2026-05-05 12:27:11 +0100

    feat(octtree): Add PolarsOctTree class

--- geotrees/octtree.py
+++ geotrees/octtree.py
@@ -20,10 +20,25 @@ neighbours.
 """
 
 import datetime
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple
+from warnings import warn
 
+import polars as pl
+
+from geotrees.distance_metrics import haversine_polars
 from geotrees.record import SpaceTimeRecord
 from geotrees.shape import SpaceTimeEllipse, SpaceTimeRectangle
+from geotrees.utils import FailedInsertWarning
+
+
+_MIN_SCHEMA = pl.Schema(
+    {
+        "uid": pl.String,
+        "lat": pl.Float64,
+        "lon": pl.Float64,
+        "datetime": pl.Datetime("us"),
+    }
+)
 
 
 class OctTree:
@@ -478,3 +493,459 @@ class OctTree:
             )
 
         return points
+
+
+class PolarsOctTree:
+    """Polars version of OctTree"""
+
+    def __init__(
+        self,
+        boundary: SpaceTimeRectangle,
+        capacity: int = 5,
+        depth: int = 0,
+        max_depth: Optional[int] = None,
+    ) -> None:
+        self.boundary = boundary
+        self.capacity = capacity
+        self.depth = depth
+        self.max_depth = max_depth
+        self.data: Optional[pl.DataFrame] = None
+        self.divided: bool = False
+        return None
+
+    @property
+    def uids(self) -> list[str]:
+        """UIDS from the data"""
+        if self.data is None:
+            return []
+        return self.data.get_column("uid").to_list()
+
+    def __str__(self) -> str:
+        indent = "    " * self.depth
+        out = f"{indent}OctTree:\n"
+        out += f"{indent}- boundary: {self.boundary}\n"
+        out += f"{indent}- capacity: {self.capacity}\n"
+        out += f"{indent}- depth: {self.depth}\n"
+        if self.max_depth:
+            out += f"{indent}- max_depth: {self.max_depth}\n"
+        if self.data is not None:
+            out += f"{indent}- contents:\n"
+            out += f"{indent}- number of elements: {self.data.height}\n"
+            out += f"{indent}- {self.data}\n"
+        if self.divided:
+            out += f"{indent}- with branches:\n"
+            out += f"{self.northwestback}"
+            out += f"{self.northeastback}"
+            out += f"{self.southwestback}"
+            out += f"{self.southeastback}"
+            out += f"{self.northwestfwd}"
+            out += f"{self.northeastfwd}"
+            out += f"{self.southwestfwd}"
+            out += f"{self.southeastfwd}"
+        return out
+
+    def len(self, current_len: int = 0) -> int:
+        """Get the number of points in the OctTree"""
+        # Points are only in leaf nodes
+        if not self.divided:
+            if self.data is None:
+                return current_len
+            return current_len + self.data.height
+
+        if self.data is not None:
+            current_len += self.data.height
+
+        for branch in self.branches:
+            current_len = branch.len(current_len)
+
+        return current_len
+
+    def divide(self):
+        """Divide the OctTree"""
+        self.northwestfwd = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.west,
+                self.boundary.lon,
+                self.boundary.lat,
+                self.boundary.north,
+                self.boundary.centre_datetime,
+                self.boundary.end,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.northeastfwd = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.lon,
+                self.boundary.east,
+                self.boundary.lat,
+                self.boundary.north,
+                self.boundary.centre_datetime,
+                self.boundary.end,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.southwestfwd = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.west,
+                self.boundary.lon,
+                self.boundary.south,
+                self.boundary.lat,
+                self.boundary.centre_datetime,
+                self.boundary.end,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.southeastfwd = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.lon,
+                self.boundary.east,
+                self.boundary.south,
+                self.boundary.lat,
+                self.boundary.centre_datetime,
+                self.boundary.end,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.northwestback = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.west,
+                self.boundary.lon,
+                self.boundary.lat,
+                self.boundary.north,
+                self.boundary.start,
+                self.boundary.centre_datetime,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.northeastback = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.lon,
+                self.boundary.east,
+                self.boundary.lat,
+                self.boundary.north,
+                self.boundary.start,
+                self.boundary.centre_datetime,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.southwestback = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.west,
+                self.boundary.lon,
+                self.boundary.south,
+                self.boundary.lat,
+                self.boundary.start,
+                self.boundary.centre_datetime,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.southeastback = PolarsOctTree(
+            SpaceTimeRectangle(
+                self.boundary.lon,
+                self.boundary.east,
+                self.boundary.south,
+                self.boundary.lat,
+                self.boundary.start,
+                self.boundary.centre_datetime,
+            ),
+            capacity=self.capacity,
+            depth=self.depth + 1,
+            max_depth=self.max_depth,
+        )
+        self.divided = True
+        self.branches: list[PolarsOctTree] = [
+            self.northwestback,
+            self.northeastback,
+            self.southwestback,
+            self.southeastback,
+            self.northwestfwd,
+            self.northeastfwd,
+            self.southwestfwd,
+            self.southeastfwd,
+        ]
+        self.redistribute_to_branches()
+
+    def _partition_data_wrapped(
+        self,
+        data: pl.DataFrame,
+    ) -> Dict[Tuple[int,], pl.DataFrame]:
+        mid_lat = self.boundary.lat
+        mid_lon = self.boundary.lon
+        mid_date = self.boundary.centre_datetime
+
+        res = data.with_columns(
+            pl.col("lat").ge(mid_lat).alias("_cond_gt_lat"),
+            pl.col("datetime").gt(mid_date).alias("_cond_gt_dt"),
+        )
+        if self.boundary.east > mid_lon:
+            res = res.with_columns(
+                pl.col("lon")
+                .is_between(mid_lon, self.boundary.east, closed="right")
+                .alias("_cond_gt_lon")
+            )
+        else:
+            res = res.with_columns(
+                pl.col("lon")
+                .is_between(self.boundary.west, mid_lon, closed="right")
+                .alias("_cond_gt_lon")
+            )
+        # Order is NW, NE, SW, SE, BCK, FWD
+        # Make bit value:
+        # 0 (000) is NWB, 1 (001) is NEB, 2 (010) is SWB, 3 (011) is SEB
+        # 4 (100) is NWF, 5 (101) is NEF, 6 (110) is SWF, 7 (111) is SEF
+        res = res.with_columns(
+            (
+                pl.col("_cond_gt_lon")
+                + 2 * ~pl.col("_cond_gt_lat")
+                + 4 * pl.col("_cond_gt_dt")
+            ).alias("_cond")
+        ).drop("_cond_gt_lon", "_cond_gt_lat", "_cond_gt_dt")
+        return res.partition_by("_cond", as_dict=True)
+
+    def _partition_data(
+        self,
+        data: pl.DataFrame,
+    ) -> Dict[Tuple[int,], pl.DataFrame]:
+        if self.boundary.wraps:
+            return self._partition_data_wrapped(data=data)
+        mid_lat = self.boundary.lat
+        mid_lon = self.boundary.lon
+        mid_date = self.boundary.centre_datetime
+
+        # Order is NW, NE, SW, SE, BCK, FWD
+        # Make bit value:
+        # 0 (000) is NWB, 1 (001) is NEB, 2 (010) is SWB, 3 (011) is SEB
+        # 4 (100) is NWF, 5 (101) is NEF, 6 (110) is SWF, 7 (111) is SEF
+        res = (
+            data.with_columns(
+                (pl.col("lon").gt(mid_lon)).alias("_cond_gt_lon"),
+                (pl.col("lat").ge(mid_lat)).alias("_cond_gt_lat"),
+                pl.col("datetime").gt(mid_date).alias("_cond_gt_dt"),
+            )
+            .with_columns(
+                (
+                    pl.col("_cond_gt_lon")
+                    + 2 * ~pl.col("_cond_gt_lat")
+                    + 4 * pl.col("_cond_gt_dt")
+                ).alias("_cond")
+            )
+            .drop("_cond_gt_lon", "_cond_gt_lat", "_cond_gt_dt")
+        )
+        return res.partition_by("_cond", as_dict=True)
+
+    def redistribute_to_branches(self) -> bool:
+        """Redistribute all points to branches"""
+        if not self.divided:
+            self.divide()
+        if self.data is None or self.data.height == 0:
+            return False
+        data_sp = self._partition_data(self.data)
+        for i, frame in data_sp.items():
+            index = int(i[0])
+            self.branches[index].insert(frame.drop("_cond", strict=False))
+        self.data = None
+        return True
+
+    def insert_into_branches(self, data: pl.DataFrame) -> None:
+        """Redistribute all points to branches"""
+        if not self.divided:
+            self.divide()
+        data_sp = self._partition_data(data)
+        for i, frame in data_sp.items():
+            index = int(i[0])
+            self.branches[index].insert(frame.drop("_cond", strict=False))
+        return None
+
+    def _check_bounds(
+        self,
+        data: Optional[pl.DataFrame] = None,
+        rectangle: Optional[SpaceTimeRectangle] = None,
+    ) -> pl.Series:
+        if rectangle is None:
+            rectangle = self.boundary
+        if data is None:
+            data = self.data
+        if data is None:
+            raise ValueError(
+                "No Data was passed, could not compare to rectangle"
+            )
+
+        return rectangle.check_frame(data)
+
+    def _check_valid(
+        self, data: pl.DataFrame
+    ) -> Tuple[pl.DataFrame, pl.DataFrame]:
+        """
+        Returns frame with records in boundary, frame with records outside
+        boundary.
+        """
+        if not all(c in data.columns for c in ["uid", "lat", "lon"]):
+            raise ValueError("Require all 'uid', 'lat' and 'lon' columns.")
+
+        data = data.cast(_MIN_SCHEMA, strict=True)
+
+        data = data.with_columns(
+            pl.col("lon").alias("_olon"),
+            (((pl.col("lon") + 540) % 360) - 180).alias("lon"),
+        )
+        valid = self._check_bounds(data=data)
+        return data.filter(valid), data.remove(valid)
+
+    def query(
+        self,
+        rect: SpaceTimeRectangle,
+        frame: Optional[pl.DataFrame] = None,
+    ) -> pl.DataFrame:
+        """Query with a rectangle"""
+        if frame is None:
+            frame = pl.DataFrame(schema=_MIN_SCHEMA)
+
+        if not self.boundary.intersects(rect):
+            return frame
+
+        if self.data is not None:
+            in_rect = rect.check_frame(self.data)
+            frame = pl.concat(
+                [frame, self.data.filter(in_rect)], how="diagonal"
+            )
+
+        if self.divided:
+            for branch in self.branches:
+                frame = branch.query(rect, frame=frame)
+
+        return frame
+
+    def query_ellipse(
+        self,
+        ellipse: SpaceTimeEllipse,
+        frame: Optional[pl.DataFrame] = None,
+    ) -> pl.DataFrame:
+        """Query the Tree with an Ellipse"""
+        # TODO: Get this working
+        raise NotImplementedError(
+            "Querying a PolarsQuadTree with an Ellipse is not yet implemented."
+        )
+
+    def nearby_points(
+        self,
+        lat: float,
+        lon: float,
+        datetime: datetime.datetime,
+        dist: float,
+        t_dist: datetime.timedelta,
+        frame: Optional[pl.DataFrame] = None,
+    ) -> pl.DataFrame:
+        """Query with a distance"""
+        lon = ((540 + lon) % 360) - 180
+        if frame is None:
+            frame = pl.DataFrame(schema=_MIN_SCHEMA)
+
+        if not self.boundary.nearby(
+            SpaceTimeRecord(lat=lat, lon=lon, datetime=datetime),
+            dist=dist,
+            t_dist=t_dist,
+        ):
+            return frame
+
+        if self.data is None and not self.divided:
+            return frame
+
+        if self.data is not None:
+            candidates = haversine_polars(self.data, lat=lat, lon=lon)
+            candidates = candidates.filter(pl.col("_dist").le(dist))
+
+            if candidates.height > 0:
+                frame = pl.concat([frame, candidates], how="diagonal")
+
+        if self.divided:
+            for branch in self.branches:
+                frame = branch.nearby_points(
+                    lat=lat,
+                    lon=lon,
+                    datetime=datetime,
+                    dist=dist,
+                    frame=frame,
+                    t_dist=t_dist,
+                )
+
+        return frame
+
+    def insert(self, data: pl.DataFrame, strict: bool = False) -> bool:
+        """
+        Insert a point into the QuadTree.
+
+        Parameters
+        ----------
+        data : polars.DataFrame
+            The data to insert
+
+        Returns
+        -------
+        bool
+            True if the point was inserted into the QuadTree
+        """
+        if self.depth == 0:
+            # Don't need to do this for lower levels, as this validation
+            # automatically propagates (unless this is called directly for a
+            # branch...)
+            data, bad = self._check_valid(data)
+            msg = (
+                f"Have {bad.height} rows of data that cannot be inserted, "
+                + "outside boundary."
+            )
+            if bad.height > 0:
+                if strict:
+                    raise ValueError(msg)
+                # Raise?
+                warn(msg, FailedInsertWarning)
+
+        if data.height == 0:
+            return False
+
+        if self.divided:
+            self.insert_into_branches(data)
+            return True
+
+        self.data = data if self.data is None else pl.concat([self.data, data])
+
+        if self.max_depth and self.depth == self.max_depth:
+            return True
+
+        if self.data.height > self.capacity:
+            self.divide()
+
+        return True
+
+    def remove(
+        self, uid: str, lat: float, lon: float, datetime: datetime.datetime
+    ) -> bool:
+        """Remove a value from the Tree"""
+        lon = ((540 + lon) % 360) - 180
+        r = SpaceTimeRecord(lon=lon, lat=lat, datetime=datetime)
+        if not self.boundary.contains(r):
+            return False
+        if self.data is not None:
+            uids_have = self.data.get_column("uid")
+            if uid in uids_have:
+                self.data = self.data.remove(uid=uid)
+                return True
+        if self.divided:
+            for branch in self.branches:
+                if branch.remove(uid, lat, lon, datetime):
+                    return True
+
+        return False
